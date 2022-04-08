504a5d4a9be8a826e0e68b48381d0e64bf1b2782
huzong <kinghyl13@gmail.com>
2022-04-08 01:59:25 -0500

    crash after write, then read

--- src/client/client.cc
+++ src/client/client.cc
@@ -163,105 +163,15 @@ std::string strRand(int length) {
     return buffer;
 }
 
-void writeToZeroOffset(BlockStorageClient *client, char *buffer) {
-    client->Write(0, buffer, BLOCK_SIZE);
-}
-
-void consistencyTest(BlockStorageClient &client, std::vector<std::string> &random_strs) {
-    std::vector<std::thread> threads;
-    for (size_t i = 0; i < random_strs.size(); i++) {
-        std::string an_input_string = random_strs[i];
-        char buffer_in[BLOCK_SIZE] = {};
-        std::memcpy(buffer_in, an_input_string.data(), an_input_string.length());
-        threads.push_back(std::thread(writeToZeroOffset, &client, buffer_in));
-    }
-
-    for (size_t i = 0; i < threads.size(); i++) {
-        threads[i].join();
-    }
-
-    // returned string should match with one of the strings which was writtern by one thread
-    for (size_t i = 0; i < random_strs.size(); i++) {
-        std::string an_input_string = random_strs[i];
-        char buffer_out[BLOCK_SIZE] = {};
-        client.Read(0, buffer_out, BLOCK_SIZE);
-        std::string returned(buffer_out, an_input_string.length());
-        if (returned == an_input_string) {
-            cout << "Matched with " << i + 1 << "th string" << endl;
-            return;
-        }
-    }
-    cout << "No match" << endl;
-}
-
-void readWriteBenchmark(BlockStorageClient &client, std::vector<std::string> &random_strs, std::vector<long> &random_offsets) {
-    // std::string an_input_string("This is tesing!!!");
-    for (size_t i = 0; i < random_strs.size(); i++) {
-        std::string an_input_string = random_strs[i];
-        char buffer_in[BLOCK_SIZE] = {};
-        char buffer_out[BLOCK_SIZE] = {};
-
-        std::memcpy(buffer_in, an_input_string.data(), an_input_string.length());
-
-        uint64_t addr = random_offsets[i];
-
-        auto start = std::chrono::high_resolution_clock::now();
-        client.Write(addr, buffer_in, BLOCK_SIZE);
-        client.Read(addr, buffer_out, BLOCK_SIZE);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> duration = end - start;
-
-        std::string returned(buffer_out, an_input_string.length());
-
-        if (an_input_string == returned) {
-            cout << "String length: " << an_input_string.length() << " write & read successful! and time spent: " << duration.count() << " ms." << endl;
-        } else {
-            cout << "write & read don't match!" << endl;
-        }
-    }
-    // measuring time for 100 writes
-    //  std::string KB_input_string = random_strs[5];
-    //  char buffer_in_4KB[BLOCK_SIZE] = {};
-    //  char buffer_out_4KB[BLOCK_SIZE] = {};
-    //  std::memcpy(buffer_in_4KB, KB_input_string.data(), KB_input_string.length());
-
-    // uint64_t offset;
-    // auto start = std::chrono::high_resolution_clock::now();
-    // for (int i = 0; i < 100; i++)
-    // {
-
-    // client->Write(offset, buffer_in_4KB, BLOCK_SIZE);
-    // offset += 4096;
-    // }
-    //  auto end = std::chrono::high_resolution_clock::now();
-    //  std::chrono::duration<double, std::milli> duration = end - start;
-    //  cout << "time spent on 100 4KB writes  " << duration.count() << " ms." << endl;
-
-    // measuring time for 100 reads
-
-    // offset = 0;
-    // auto start = std::chrono::high_resolution_clock::now();
-    // for (int i = 0; i < 100; i++)
-    // {
-
-    // client.Read(offset, buffer_out_4KB, BLOCK_SIZE);
-    // offset += 4096;
-    // }
-    //  auto end = std::chrono::high_resolution_clock::now();
-    //  std::chrono::duration<double, std::milli> duration = end - start;
-    //  cout << "time spent on 100 4KB reads  " << duration.count() << " ms." << endl;
-}
-
-
 void run_main(BlockStorageClient *client, uint64_t prep, uint64_t target) {
     for (int i = 0; i < 10; i++) {
         auto payload1 = strRand(BLOCK_SIZE);
         auto payload2 = strRand(BLOCK_SIZE);
         char buffer_out[BLOCK_SIZE] = {};
 
+        auto start = std::chrono::high_resolution_clock::now();
         client->Write(prep, payload1.c_str(), BLOCK_SIZE);
-        auto start = std::chrono::high_resolution_clock::now();
-        client->Write(target, payload2.c_str(), BLOCK_SIZE);
+        // client->Write(target, payload2.c_str(), BLOCK_SIZE);
         client->Read(target, buffer_out, BLOCK_SIZE);
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration = end - start;
@@ -274,37 +184,6 @@ void run_main(BlockStorageClient *client, uint64_t prep, uint64_t target) {
             cout << "[iter " << i << "] inconsistent!" << endl;
         }
     }
-}
-
-void run_rest(BlockStorageClient *client, uint64_t prep, uint64_t target) {
-    for (int i = 0; i < 10; i++) {
-        auto payload1 = strRand(BLOCK_SIZE);
-        auto payload2 = strRand(BLOCK_SIZE);
-        auto payload3 = strRand(BLOCK_SIZE);
-        char buffer_out[BLOCK_SIZE] = {};
-
-        client->Write(PREP_CRASH_ON_NEXT_RECOVER, payload1.c_str(), BLOCK_SIZE);
-        client->Write(prep, payload2.c_str(), BLOCK_SIZE);
-        auto start = std::chrono::high_resolution_clock::now();
-        client->Write(target, payload2.c_str(), BLOCK_SIZE);
-        client->Read(target, buffer_out, BLOCK_SIZE);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> duration = end - start;
-
-        std::string returned(buffer_out, payload2.length());
-
-        if (payload2 == returned) {
-            cout << "[iter " << i << "] OK: took " << duration.count() << "ms" << endl;
-        } else {
-            cout << "[iter " << i << "] inconsistent!" << endl;
-        }
-    }
-}
-
-
-void seq0(BlockStorageClient *client) {
-    cout << "Crashing primary during write, before calling backup" << endl;
-    run_main(client,PREP_CRASH_ON_MESSAGE_PRIMARY, CRASH_PRIMARY_BEFORE_BACKUP);
 }
 
 void seq1(BlockStorageClient* client) {
@@ -312,69 +191,6 @@ void seq1(BlockStorageClient* client) {
     run_main(client,PREP_CRASH_ON_MESSAGE_PRIMARY, CRASH_PRIMARY_AFTER_WRITE);
 }
 
-void seq2(BlockStorageClient *client) {
-    cout << "Crashing backup during backup" << endl;
-    run_main(client,PREP_CRASH_ON_MESSAGE_BACKUP, CRASH_BACKUP_DURING_BACKUP);
-}
-
-void seq3(BlockStorageClient *client) {
-    cout << "Crashing backup after backup" << endl;
-    run_main(client,PREP_CRASH_ON_MESSAGE_BACKUP, CRASH_BACKUP_AFTER_BACKUP);
-}
-
-void seq4(BlockStorageClient *client) {
-    cout << "Crashing primary once, then again as it recovers" << endl;
-    run_rest(client,PREP_CRASH_ON_MESSAGE_PRIMARY, CRASH_PRIMARY_BEFORE_BACKUP);
-}
-
-void seq5(BlockStorageClient *client) {
-    cout << "Crashing backup once, then again as it recovers" << endl;
-    run_rest(client,PREP_CRASH_ON_MESSAGE_BACKUP, CRASH_BACKUP_DURING_BACKUP);
-}
-
-void runTests(BlockStorageClient &client) {
-    // std::vector<long> str_lengths = {64, 256, 512, BLOCK_SIZE / 4, BLOCK_SIZE / 2, BLOCK_SIZE};
-    std::vector<long> str_lengths = {BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE};
-    std::vector<std::string> random_strs;
-    std::vector<long> random_offsets;
-    for (auto &&i : str_lengths) {
-        random_strs.push_back(strRand(i));
-    }
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < str_lengths.size(); i++) {
-        // random_offsets.push_back(rand()); // unaligned
-        random_offsets.push_back(i * BLOCK_SIZE);  // 4k-aligned
-    }
-
-    readWriteBenchmark(client, random_strs, random_offsets);
-    // int testNum = 5;
-    // for (size_t i = 0; i < testNum; i++)
-    // {
-    //     consistencyTest(client, random_strs);
-    // }
-
-    // long [9] = {};
-    // string
-    // long offsets[]
-
-    // int k = an_input_string.length();
-
-    // cout << "Sent " << std::hex;
-    // for (int i = 0; i < k; i++)
-    // {
-    //     cout << std::setfill('0') << std::setw(2) << (0xff & (unsigned int)buffer_in[i]) << " ";
-    // }
-    // cout << std::dec << endl;
-
-    // cout << "Got  " << std::hex;
-    // for (int i = 0; i < k; i++)
-    // {
-    //     cout << std::setfill('0') << std::setw(2) << (0xff & (unsigned int)buffer_out[i]) << " ";
-    // }
-    // cout << std::dec << endl;
-
-    // printf("Sent [%s], got back [%s]\n", an_input_string.c_str(), returned.c_str());
-}
 
 int main(int argc, char **argv) {
     std::string target_str_1(INSTANCE_5_IP);
@@ -384,16 +200,7 @@ int main(int argc, char **argv) {
         grpc::CreateChannel(target_str_1, grpc::InsecureChannelCredentials()),
         grpc::CreateChannel(target_str_2, grpc::InsecureChannelCredentials()));
     
-    auto n = atoi(argv[1]);
-    switch(n) {
-        case 0: seq0(&client);break;
-        case 1: seq1(&client);break;
-        case 2: seq2(&client);break;
-        case 3: seq3(&client);break;
-        case 4: seq4(&client);break;
-        case 5: seq5(&client);break;
-        default: return 1;
-    }
+    seq1(&client);
     
     return 0;
 }
